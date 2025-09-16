6060d6f1ce7433408182e948b322cdd9f971869f
cd ~/code/$GITHUB_USERNAME/dotfiles && zsh git_setup.sh <jeremyarcafire@gmail.com>
2025-09-16 16:33:27 -0500

    rails new

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,47 @@
+class TasksController < ApplicationController
+before_action :set_task, only: %i[show edit update destroy]
+
+  def index
+    @tasks = Task.all.order(created_at: :desc)
+  end
+
+  def show; end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to @task, notice: "Task creada correctamente."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit; end
+
+  def update
+    if @task.update(task_params)
+      redirect_to @task, notice: "Task actualizada."
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @task.destroy
+    redirect_to tasks_path, notice: "Task eliminada."
+  end
+
+  private
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
+end
